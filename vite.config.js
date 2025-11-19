33899565cae9ead7e02acb8f8c7254db2f9eb661
Absalón <77087918@continental.edu.pe>
2025-11-19 18:23:09 -0500

    Configurar Vite correctamente para producción en Railway

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,16 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            input: {
+                app: 'resources/js/app.js',
+                css: 'resources/css/app.css'
+            }
+        }
+    },
     server: {
         host: 'localhost',
         port: 5173,
